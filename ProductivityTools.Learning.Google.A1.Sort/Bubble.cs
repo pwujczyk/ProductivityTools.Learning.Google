0612343d315e4680c905e2efa0da7a20851889b2
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-05-30 21:56:53 +0200

    selection sort

---
+++ ProductivityTools.Learning.Google.A1.Sort/Bubble.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductivityTools.Learning.Google.A1.Sort
+{
+    class Bubble
+    {
+        public int Sort(int[] a)
+        {
+            int result = 0;
+            bool correctOrder = false;
+            while(correctOrder!=true)
+            {
+                correctOrder = true;
+                for (int i = 0; i < a.Length-1; i++)
+                {
+                    result++;
+                    if(a[i]>a[i+1])
+                    {
+                        int temp = a[i + 1];
+                        a[i + 1] = a[i];
+                        a[i] = temp;
+                        correctOrder = false;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
--- ProductivityTools.Learning.Google.A1.Sort/Program.cs
+++ ProductivityTools.Learning.Google.A1.Sort/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ProductivityTools.Learning.Google.A1.Sort
 {
@@ -7,6 +8,24 @@ namespace ProductivityTools.Learning.Google.A1.Sort
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            int[] table = new int[10];
+            for (int i = 0; i < table.Length; i++)
+            {
+                table[i] = new Random().Next(10);
+            }
+            Print(table);
+            var r=new Bubble().Sort(table);
+            Console.WriteLine($"Amount{r}");
+            Print(table);
+        }
+
+        static void Print(int[] table)
+        {
+            Console.WriteLine("===");
+            foreach (var item in table)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
