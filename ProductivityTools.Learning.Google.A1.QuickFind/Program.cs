894101ff1387beca13d5420fef7b3a6e4c6e2152
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-05-30 10:07:09 +0200

    quick find alghoritm

---
+++ ProductivityTools.Learning.Google.A1.QuickFind/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ProductivityTools.Learning.Google.A1.QuickFind
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            QF qf = new QF(4);
+            qf.PrintTable();
+            Console.WriteLine(qf.Connected(1, 2));
+
+            Console.WriteLine();
+            qf.Union(1, 2);
+            qf.PrintTable();
+            Console.WriteLine(qf.Connected(1, 2));
+
+
+            Console.WriteLine();
+            qf.Union(0, 2);
+            qf.PrintTable();
+            Console.WriteLine(qf.Connected(0, 1));
+
+            Console.Read();
+        }
+    }
+}
