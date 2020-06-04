44a42732810637db2b6ad0ac9ac529c0a7a175a5
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-06-04 19:04:09 +0200

    pprioruty quene

---
+++ ProductivityTools.Learning.Google.PriorityQuene/PriorityQuene.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProductivityTools.Learning.Google.PriorityQuene
+{
+    class PriorityQuene:List<int>
+    {
+        private readonly int Max;
+
+        public PriorityQuene(int max)
+        {
+            this.Max = max;
+        }
+
+        public void RemoveMin()
+        {
+            var x = this.Min();
+            this.Remove(x);
+        }
+
+        internal void AddMax(int z)
+        {
+            if(this.Count>Max)
+            {
+                //sort by insert for example
+                //isnert if greater
+            }
+         
+
+        }
+    }
+}
--- ProductivityTools.Learning.Google.PriorityQuene/Program.cs
+++ ProductivityTools.Learning.Google.PriorityQuene/Program.cs
@@ -6,6 +6,14 @@ namespace ProductivityTools.Learning.Google.PriorityQuene
     {
         static void Main(string[] args)
         {
+            var x=new PriorityQuene(10);
+
+            for (int i = 0; i < 10000; i++)
+            {
+                int z = new Random().Next(10);
+                x.AddMax(z);
+            }
+
             Console.WriteLine("Hello World!");
         }
     }
