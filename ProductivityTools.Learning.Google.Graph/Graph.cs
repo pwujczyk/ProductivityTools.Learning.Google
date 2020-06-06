d8b23e9fb8b5d6e7b1182fd6a38dbc5345443dac
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-06-06 12:55:29 +0200

    Graph

---
+++ ProductivityTools.Learning.Google.Graph/Graph.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.PortableExecutable;
+using System.Text;
+
+namespace ProductivityTools.Learning.Google.Graph
+{
+    class Graph
+    {
+        private int V;
+        public List<int>[] adj;
+
+        public Graph(int v)
+        {
+            this.V = v;
+            for (int i = 0; i < V; i++)
+            {
+                this.adj[i]=new List<int>();
+            }
+        }
+
+        public void AddEdge(int v, int w)
+        {
+            adj[v].Add(w);
+            adj[w].Add(v);
+        }
+    }
+}
