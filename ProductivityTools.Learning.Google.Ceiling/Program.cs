7a6998c24bc51abb83ad4ff20a444ea3ed332a31
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-06-06 09:26:18 +0200

    find floor

---
+++ ProductivityTools.Learning.Google.Ceiling/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ProductivityTools.Learning.Google.Ceiling
+{
+    class node
+    {
+        int value;
+        public node left;
+        public node right;
+        public node(int value)
+        {
+            this.value = value;
+        }
+
+        public int FindFloor(int value)
+        {
+            if (this.value == value) { return value; }
+            if (value < this.value)
+            {
+                if (left == null) return this.value;
+                return this.left.FindFloor(value);
+            }
+            if (this.value < value)
+            {
+                if (this.right == null) return this.value;
+                return this.right.FindFloor(value);
+            }
+            throw new Exception("Something went wrong");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            node root = new node(100);
+            root.left = new node(50);
+            root.right = new node(200);
+            node current = root.left;
+            current.left = new node(3);
+            current.right = new node(70);
+
+
+            var r=root.FindFloor(60);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
