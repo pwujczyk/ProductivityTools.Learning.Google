c289cd586d0fe3ff25de4c063e7f5f45d761503f
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-12-14 21:52:41 +0100

    prim

---
+++ ProductivityTools.Learning.Google.Prim/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProductivityTools.Learning.Google.Prim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
